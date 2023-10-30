f752a45ae8c057f7df5b6a64c465fce741d42749
Henry Wilt <henrywwilt@gmail.com>
2023-10-30 23:33:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/skills-member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+        
+    }
+}
