1b38b7f2ce4db65281633d336c25f6db92fd69a0
Henry Wilt <henrywwilt@gmail.com>
2023-10-31 00:31:43 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,32 @@
+// create a web server
+// 1. create a web server
+// 2. handle requests
+// 3. return a response
+// 4. listen on a port
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const port = 3000;
+
+// create a web server
+const server = http.createServer((req, res) => {
+    // handle requests
+    const path = url.parse(req.url, true);
+    const filename = `.${path.pathname}.json`;
+    console.log(filename);
+    // return a response
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(data);
+        return res.end();
+    });
+});
+
+// listen on a port
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
